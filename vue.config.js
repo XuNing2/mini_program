2f11ea0aea19f3fd146a82182e9172cd1c86066a
XuNing2 <xuning446@gmail.com>
2022-01-13 16:26:29 +0800

    vue.config.js 修改

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,35 @@
-// const proxyObj = {};
-// proxyObj["/"] = {
-//     target: "http://z-hand.site/",
-//     changeOrigin: true,
-//     pathRewrite: {
-//         "^/": "",
-//     },
-// };
-// module.exports = {
-//     devServer: {
-//         host: "localhost",
-//         port: 8080,
-//         proxy: proxyObj,
-//     },
-// };
-
 //vue.config.js
 
 module.exports = {
+    publicPath: "./",
+    outputDir: "dist",
+
+    lintOnSave: true,
+    runtimeCompiler: true, //关键点在这
+    // 调整内部的 webpack 配置。
+    // chainWebpack: (config) => {
+    //   config.resolve.alias
+    //   //set第一个参数：设置的别名，第二个参数：设置的路径
+    //   .set('@',resolve('./src'))
+    //   .set('components',resolve('./src/components'))
+    //   .set('assets',resolve('./src/assets'))
+    //   .set('views',resolve('./src/views'))
+    // },
+    // configureWebpack: () => {},
+    // 配置 webpack-dev-server 行为。
     devServer: {
+        //   open: process.platform === 'darwin',
+        host: "127.0.0.1",
         port: 8080,
+        https: false,
+        hotOnly: false,
         proxy: {
-            //配置跨域
             "/api": {
-                target: "https://z-hand.site", //这里填写项目真实的后台接口地址
-                changOrigin: true, //设置允许跨域
+                // 请求的目标地址的BaseURL
+                target: "https://z-hand.site/",
+                changeOrigin: true, //是否开启跨域
                 pathRewrite: {
-                    //这个重写不可省略！因为我们真正请求的地址并不含 /api
-                    "^/api": "",
-                    /* 当我们在浏览器中看到请求的地址为：http://localhost:8080/api/data/getdata 时
-                    因为重写了 /api ，所以实际上访问的地址是：http://x.x.x.x:x/data/getdata，
-                    */
+                    "^/api": "", //规定请求地址以什么作为开头
                 },
             },
         },
